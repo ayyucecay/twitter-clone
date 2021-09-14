d0dbda64f201fb1afd39e0ec5a8f3cc0d1876ac5
ayyucecay <61210641+ayyucecay@users.noreply.github.com>
2021-09-14 09:57:33 +0300

    create page

---
+++ explore_page/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExplorePageConfig(AppConfig):
+    name = 'explore_page'
---
+++ notifications_page/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NotificationsPageConfig(AppConfig):
+    name = 'notifications_page'
